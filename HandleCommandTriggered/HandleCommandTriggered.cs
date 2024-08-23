54be08ee8846b3c2f47fa064920f92d61f8f10b0
Jelle Van Goethem <jelle.van.goethem@hotmail.com>
2024-08-23 03:29:23 +0200

    Import my HandleCommandTriggered script from Streamer.bot

---
+++ HandleCommandTriggered/HandleCommandTriggered.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using Streamer.bot.Plugin.Interface; // Remove in Streamer.bot
+using Streamer.bot.Plugin.Interface.Enums; // Remove in Streamer.bot
+using Streamer.bot.Plugin.Interface.Model; // Remove in Streamer.bot
+
+/*
+    Use this template for Streamer.bot C# code -> copy all code here and paste in Streamer.bot but remove
+    marked 'using' Streamer.bot... and remove ": CPHInlineBase" out of "public class CPHInline : CPHInlineBase"
+*/
+
+public class CPHInline : CPHInlineBase // Remove ": CPHInlineBase" in Streamer.bot
+{
+    public bool Execute()
+    {
+        CPH.TryGetArg("commandId", out Guid commandId);
+        CPH.TryGetArg("rawInput", out string rawInput);
+        CPH.TryGetArg("user", out string user);
+        CPH.TryGetArg("broadcastUserName", out string broadcastUserName);
+
+        string commandIdString = commandId.ToString();
+
+        switch (commandIdString)
+        {
+            // Utilities
+
+            case "a05dbd68-79a0-4d5f-91fc-ac152b3ba70a": // Clip
+                CPH.CreateClip();
+
+                CPH.Wait(5000);
+
+                CPH.TryGetArg("createClipSuccess", out bool createClipSuccess);
+
+                if (createClipSuccess == false)
+                {
+                    CPH.SendAction($"@{user} Failed to create clip TearGlove");
+
+                    break;
+                }
+
+                CPH.TryGetArg("createClipUrl", out string createClipUrl);
+
+                if (string.IsNullOrEmpty(createClipUrl))
+                {
+                    CPH.SendAction(
+                        $"@{user} Successfully created clip but failed to get the URL. You should find the clip at https://www.twitch.tv/{broadcastUserName}/clips?filter=clips&range=7d"
+                    );
+                }
+                else
+                {
+                    CPH.SendAction($"@{user} Successfully created clip -> {createClipUrl}");
+                }
+
+                break;
+
+            case "64fe6caf-1fab-46eb-aeeb-17558db811ca": // Shoutout
+                if (string.IsNullOrEmpty(rawInput))
+                {
+                    CPH.SendAction($"@{user} No user provided -> Usage: !shoutout/!so @user/user");
+
+                    break;
+                }
+
+                TwitchUserInfoEx twitchUserInfoEx = CPH.TwitchGetExtendedUserInfoByLogin(
+                    rawInput.Replace("@", "")
+                );
+
+                if (twitchUserInfoEx == null)
+                {
+                    CPH.SendAction($"@{user} User doesn't exist TearGlove");
+
+                    break;
+                }
+
+                CPH.TwitchSendShoutoutByLogin(twitchUserInfoEx.UserLogin);
+
+                CPH.Wait(1500);
+
+                /*
+                    I currently don't have a way to check whether the user ever streamed.
+                    You can go live without a title and without a category...
+                */
+
+                string targetUser = twitchUserInfoEx.UserName;
+                string targetUserName = twitchUserInfoEx.UserLogin;
+                string targetChannelTitle = twitchUserInfoEx.ChannelTitle;
+                string game = twitchUserInfoEx.Game;
+
+                string actionPrefix = $"BIG shoutout to @{targetUser} nycto97Love1 ";
+
+                if (string.IsNullOrEmpty(targetChannelTitle) && string.IsNullOrEmpty(game))
+                {
+                    CPH.SendAction(
+                        actionPrefix
+                            + $"You can find them over at twitch.tv/{targetUserName} nycto97Hype1"
+                    );
+                }
+                else if (string.IsNullOrEmpty(game))
+                {
+                    CPH.SendAction(
+                        actionPrefix
+                            + $"They last streamed \"{targetChannelTitle}\" over at twitch.tv/{targetUserName} nycto97Hype1"
+                    );
+                }
+                else if (string.IsNullOrEmpty(targetChannelTitle))
+                {
+                    CPH.SendAction(
+                        actionPrefix
+                            + $"They last were doing {game} over at twitch.tv/{targetUserName} nycto97Hype1"
+                    );
+                }
+                else
+                {
+                    CPH.SendAction(
+                        actionPrefix
+                            + $"They last streamed \"{targetChannelTitle}\" and were doing {game} over at twitch.tv/{targetUserName} nycto97Hype1"
+                    );
+                }
+
+                break;
+
+            // =================================================================
+
+            // Fun
+
+            case "58bd9d11-0f89-4cfc-80bd-0de6f5304982": // Hug
+            case "a49297a7-73cb-4071-b84d-c3a9b69ef74b": // Kiss
+            case "009c93bd-dff1-4617-b667-94ca27648dec": // Lick
+            case "2664ddf0-06e1-4e04-a690-63ba11b4dd6b": // Smash
+                string action =
+                    commandIdString == "58bd9d11-0f89-4cfc-80bd-0de6f5304982" ? "hugs"
+                    : commandIdString == "a49297a7-73cb-4071-b84d-c3a9b69ef74b" ? "kisses"
+                    : commandIdString == "009c93bd-dff1-4617-b667-94ca27648dec" ? "licks"
+                    : "smashes";
+
+                string suffix =
+                    commandIdString == "58bd9d11-0f89-4cfc-80bd-0de6f5304982" ? "love"
+                    : commandIdString == "a49297a7-73cb-4071-b84d-c3a9b69ef74b" ? "passion"
+                    : commandIdString == "009c93bd-dff1-4617-b667-94ca27648dec" ? "wetness"
+                    : "strength";
+
+                string emote =
+                    (
+                        commandIdString == "58bd9d11-0f89-4cfc-80bd-0de6f5304982"
+                        || commandIdString == "a49297a7-73cb-4071-b84d-c3a9b69ef74b"
+                    )
+                        ? "nycto97Love1"
+                    : commandIdString == "009c93bd-dff1-4617-b667-94ca27648dec" ? "nycto97LOL1"
+                    : "nycto97RIP1";
+
+                string soundFileName =
+                    commandIdString == "58bd9d11-0f89-4cfc-80bd-0de6f5304982"
+                        ? "Hug-Clothes-Pat_on_the_Back-6.mp3"
+                    : commandIdString == "a49297a7-73cb-4071-b84d-c3a9b69ef74b" ? "smoochykiss.mp3"
+                    : commandIdString == "009c93bd-dff1-4617-b667-94ca27648dec" ? "spong-lick.mp3"
+                    : "punch-sound-effect.mp3";
+
+                float volume =
+                    soundFileName == "Hug-Clothes-Pat_on_the_Back-6.mp3" ? 0.1f
+                    : soundFileName == "smoochykiss.mp3" ? 0.1f
+                    : soundFileName == "spong-lick.mp3" ? 0.1f
+                    : 0.1f;
+
+                Random random = new Random();
+                int randomPercentage = random.Next(0, 101);
+
+                if (string.IsNullOrEmpty(rawInput))
+                {
+                    CPH.SendAction(
+                        $"@{user} {action} themselves with {randomPercentage}% {suffix} {emote}"
+                    );
+
+                    CPH.PlaySound(@$"D:\jelle\Music\Twitch\Sounds\{soundFileName}", volume, false);
+
+                    break;
+                }
+
+                TwitchUserInfo twitchUserInfo = CPH.TwitchGetUserInfoByLogin(
+                    rawInput.Replace("@", "")
+                );
+
+                if (twitchUserInfo == null)
+                {
+                    CPH.SendAction($"@{user} User doesn't exist TearGlove");
+
+                    break;
+                }
+
+                CPH.SendAction(
+                    $"@{user} {action} @{twitchUserInfo.UserName} with {randomPercentage}% {suffix} {emote}"
+                );
+
+                CPH.PlaySound(@$"D:\jelle\Music\Twitch\Sounds\{soundFileName}", volume, false);
+
+                break;
+
+            case "06fdc6ff-62cd-4257-ac05-e09503aa4b6a": // Lurk
+                if (string.IsNullOrEmpty(rawInput))
+                {
+                    CPH.SendAction(
+                        $"Thanks for lurking @{user} nycto97Wave1 I appreciate your presence! nycto97Love1"
+                    );
+                }
+                else
+                {
+                    CPH.SendAction(
+                        $"Thanks for lurking @{user} nycto97Wave1 - \"{rawInput}\" - I appreciate your presence! nycto97Love1"
+                    );
+                }
+
+                break;
+
+            // =================================================================
+
+            // Socials/accounts/links
+
+            case "6a8c226d-c040-4e18-8f46-6daba6e2b48f": // Blizzard/Battle.net
+                CPH.SendAction("Blizzard / Battle.net -> Nycto#21256 🎮");
+                break;
+            case "e3c23b9c-ed27-494b-a7c6-8fe99b20a0c1": // Call of Duty/Activision
+                CPH.SendAction("Call of Duty / Activision -> Nycto#5536049 🎮");
+                break;
+            case "ec1c33ab-6b56-4153-a10f-c53fd083deed": // Counter-strike skins
+                CPH.SendAction(
+                    "CounterStrike2 Skins -> steamcommunity.com/id/Nycto97/inventory/#730 🔫"
+                );
+                break;
+            case "9adb405a-c250-4ef6-99ca-811a54f72dfa": // Discord
+                CPH.SendAction("Discord -> discord.gg/Y2fat6v 💜");
+                break;
+            case "65cddd4d-de0f-4f50-868a-26f6b6faadce": // EA/Origin
+                CPH.SendAction("EA / Origin -> Nycto1337 🎮");
+                break;
+            case "5561535f-e460-4c7d-84a3-f36baab3face": // Epic Games
+                CPH.SendAction("EpicGames -> Nycto97 🎮");
+                break;
+            case "80328fd5-620f-4808-a6ed-7598561ba685": // Facebook
+                CPH.SendAction("Facebook -> facebook.com/Nycto1337 💙");
+                break;
+            case "ff683f2e-c906-4de1-bdb7-d1742ad64c50": // Instagram
+                CPH.SendAction("Instagram -> instagram.com/Nycto97 💗");
+                break;
+            case "df11f4ea-42c5-48a1-8dfa-65cd7beceab1": // PlayStation
+                CPH.SendAction("PlayStation (PSN) -> Nycto97 🎮");
+                break;
+            case "34bb5dbe-684c-4cbc-8f6c-ad976d30f709": // Riot Games
+                CPH.SendAction("RiotGames -> Nycto#0420 🎮");
+                break;
+            case "2bead345-45e0-4a6c-9891-d21a60a02559": // Socials
+                CPH.SendAction("Socials -> linktr.ee/Nycto97 ✨");
+                break;
+            case "5d12757f-7fd7-4109-9fb7-e933dd636729": // Steam
+                CPH.SendAction("Steam -> steamcommunity.com/id/Nycto97 🎮");
+                break;
+            case "54714415-aa5f-4315-80d4-46c9a8f96866": // TikTok
+                CPH.SendAction("TikTok -> tiktok.com/@Nycto1337 💗");
+                break;
+            case "a30e24f3-55ad-4f88-80a6-376fb83e2ef8": // Tip/donate
+                CPH.SendAction(
+                    $"Tip/donate -> streamelements.com/{broadcastUserName}/tip nycto97Hype1 @{user} only tip what you can afford, I don't want you to get in trouble! nycto97Love1"
+                );
+                break;
+            case "6fbcbdf7-7203-4861-8f9a-bdc82c40424a": // Twitter/X
+                CPH.SendAction("Twitter (X) -> twitter.com/Nycto97 💙");
+                break;
+            case "8013b569-e4f3-432c-b0da-16cc08822561": // Ubisoft
+                CPH.SendAction("Ubisoft -> Nycto1337 🎮");
+                break;
+            case "fc078460-677e-4529-93ec-46b666d35dfc": // Xbox
+                CPH.SendAction("Xbox -> Nycto97 🎮");
+                break;
+            case "6c534184-d7af-4174-b585-5c57c5a0ab94": // YouTube
+                CPH.SendAction("YouTube -> youtube.com/Nycto97 🎬");
+                break;
+
+            // =================================================================
+        }
+
+        return true;
+    }
+}
