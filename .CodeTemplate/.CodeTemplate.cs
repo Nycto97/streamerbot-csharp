a5d446f55532e46586174f7faf52b82ce7eaad0f
Jelle Van Goethem <jelle.van.goethem@hotmail.com>
2024-08-23 02:37:25 +0200

    Initial commit

---
+++ .CodeTemplate/.CodeTemplate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Streamer.bot.Plugin.Interface; // Remove in Streamer.bot
+using Streamer.bot.Plugin.Interface.Enums; // Remove in Streamer.bot
+using Streamer.bot.Plugin.Interface.Model; // Remove in Streamer.bot
+
+/*
+    Use this template for Streamer.bot C# code -> copy all code here and paste in Streamer.bot but remove
+    marked 'using' Streamer.bot... and remove ": CPHInlineBase" out of "public class CPHInline : CPHInlineBase"
+*/
+
+public class CPHInline : CPHInlineBase // Remove ": CPHInlineBase" in Streamer.bot
+{
+    public bool Execute()
+    {
+        return true;
+    }
+}
