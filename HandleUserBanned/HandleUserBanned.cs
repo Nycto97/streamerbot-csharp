9615393acb7ab88bb8fe8fc0cfc03c9f4d545810
Jelle Van Goethem <jelle.van.goethem@hotmail.com>
2024-08-27 01:49:19 +0200

    Auto unban and re-mod bot account linked to S.b when it gets banned

--- HandleUserBanned/HandleUserBanned.cs
+++ HandleUserBanned/HandleUserBanned.cs
@@ -12,12 +12,46 @@ public class CPHInline : CPHInlineBase // Remove ": CPHInlineBase" in Streamer.b
 {
     public bool Execute()
     {
+        CPH.TryGetArg("userName", out string userName);
         CPH.TryGetArg("user", out string user);
         CPH.TryGetArg("reason", out string reason);
 
+        TwitchUserInfo sbBotInfo = CPH.TwitchGetBot();
+
+        if (userName != sbBotInfo.UserLogin)
+        {
+            CPH.SendAction(
+                $"@{user} got banned BOP{(!string.IsNullOrEmpty(reason) ? $" Reason: {reason}" : "")}"
+            );
+
+            return true;
+        }
+
         CPH.SendAction(
-            $"banned @{user} BOP{(!string.IsNullOrEmpty(reason) ? $" Reason: {reason}" : "")}"
+            $"@{user} got banned BOP{(!string.IsNullOrEmpty(reason) ? $" Reason: {reason}" : "")}",
+            false
         );
+
+        CPH.Wait(1000);
+
+        CPH.TwitchUnbanUser(userName, false);
+
+        CPH.SendAction($"unbanned @{user} Reason: Wrongfully banned", false);
+
+        CPH.Wait(1000);
+
+        CPH.TwitchAddModerator(userName);
+
+        CPH.SendAction($"re-added @{user} as moderator", false);
+
+        CPH.TryGetArg("createdByUsername", out string createdByUsername);
+
+        if (createdByUsername == "sery_bot")
+        {
+            CPH.Wait(1000);
+
+            CPH.SendMessage("bad bot");
+        }
 
         return true;
     }
